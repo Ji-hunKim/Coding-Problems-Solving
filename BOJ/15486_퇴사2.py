c032bd72fa75fd240de5ac9582bc9b7f9ec6e177
Ji-hunKim <anam4561@naver.com>
2022-07-05 03:15:14 +0900

    220705

--- BOJ/15486_퇴사2.py
+++ BOJ/15486_퇴사2.py
@@ -1,21 +1,17 @@
-import sys
+import sys 
+input = sys.stdin.readline 
 
-N = int(sys.stdin.readline())
-
-day = N
-T, P = [], []
-dp = [0]*(N+1)
-
-for i in range(N):
-    temp = list(map(int, sys.stdin.readline().split()))
-    T.append(temp[0])
-    P.append(temp[1])
-
-
-for i in range(0, N):
-    if T[i] <= N - i:
-        dp[i+T[i]] = max(dp[i+T[i]], dp[i]+P[i])
-        
-    dp[i+1] = max(dp[i+1], dp[i])
-
-print(dp[N])
+n = int(input())
+t,p = [],[]
+dp = [0] * (n+1)
+for i in range(n):
+    x,y = map(int,input().split())
+    t.append(x)
+    p.append(y)
+M = 0 
+for i in range(n):
+    M = max(M,dp[i])  
+    if i+t[i] > n :  
+        continue 
+    dp[i+t[i]] = max(M+p[i],dp[i+t[i]])
+print(max(dp))
